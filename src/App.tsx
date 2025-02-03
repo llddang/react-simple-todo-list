29eede4c63ffb729eaf431f27005bb8417447bc7
llddang <bodyness@naver.com>
2025-02-03 16:56:46 +0900

    feat: Todo 타입 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
 import Reducer from "./page/Reducer";
+import ReducerWithContext from "./page/ReducerWithContext";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reducer" element={<Reducer />} />
+        <Route path="/reducer-with-context" element={<ReducerWithContext />} />
       </Routes>
     </BrowserRouter>
   );
